59f402b15b7308d60e8e8851d16a90a7faeaad2a
Sprite_Spirit <74331153+SpriteSpirit@users.noreply.github.com>
2024-01-28 11:47:47 +0300

    HW5:
    playlist.py:
    - реализован метод get_videos_and_duration()
    - создан геттер для названия плейлиста
    - реализован метод show_best_video
    - в классе Video метод get_video_info() был преобразован в @classmethod

--- src/playlist.py
+++ src/playlist.py
@@ -11,9 +11,16 @@ class PlayList:
 
     def __init__(self, playlist_id):
         self.playlist_id = playlist_id
-        self.title = None
-        self.url = f'https: //www.youtube.com/playlist?list={self.playlist_id}'
+        self.url = f'https://www.youtube.com/playlist?list={self.playlist_id}'
+        self._title = None
         self._total_duration = None
+        # self.get_videos_and_duration()
+
+    @property
+    def title(self):
+        if self._title is None:
+            self._title = self.get_playlist_info()['items'][0]['snippet']['title']
+        return self._title
 
     @property
     def total_duration(self):
@@ -38,5 +45,83 @@ class PlayList:
 
         return request.execute()
 
+    def get_playlist_items(self):
+        """
+        Возвращает словарь с информацией по видео из плейлиста.
+        """
+        response = self.service.playlistItems().list(
+            part='contentDetails, snippet',
+            playlistId=self.playlist_id,
+            maxResults=50,
+            pageToken=self.nextPageToken
+        ).execute()
+
+        return response
+
     def get_videos_and_duration(self):
-        pass
+        """
+        Получает продолжительность всех видео и подсчитывает суммарное время продолжительности.
+        :return:
+        """
+
+        hours_pattern = re.compile(r'(\d+)H')
+        minutes_pattern = re.compile(r'(\d+)M')
+        seconds_pattern = re.compile(r'(\d+)S')
+
+        total_duration = timedelta()
+
+        while True:
+            vid_items = self.get_playlist_items()
+
+            for item in vid_items.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                self.video_ids.append(video_id)
+
+            video_response = Video.get_video_info(','.join(self.video_ids))
+
+            for item in video_response['items']:
+                duration = item['contentDetails']['duration']
+
+                hours = hours_pattern.search(duration)
+                minutes = minutes_pattern.search(duration)
+                seconds = seconds_pattern.search(duration)
+
+                hours = int(hours.group(1)) if hours else 0
+                minutes = int(minutes.group(1)) if minutes else 0
+                seconds = int(seconds.group(1)) if seconds else 0
+
+                video_duration = timedelta(
+                    hours=hours,
+                    minutes=minutes,
+                    seconds=seconds
+                )
+
+                total_duration += video_duration
+
+            self.nextPageToken = vid_items.get('nextPageToken')
+
+            if not self.nextPageToken:
+                break
+
+        self._total_duration = total_duration
+
+    def show_best_video(self):
+        """
+        Находит максимальное кол-во лайков у видео и определяет id видео
+        :return:
+        """
+        best_video_id = None
+        best_video_likes = 0
+
+        for video in self.video_ids:
+            response = Video.get_video_info(video)
+            likes_count = int(response.get('items', [])[0]['statistics']['likeCount'])
+
+            if likes_count > best_video_likes:
+                best_video_likes = likes_count
+                best_video_id = video
+
+        if best_video_id:
+            return f'https://youtu.be/{best_video_id}'
+        else:
+            return None
--- src/video.py
+++ src/video.py
@@ -6,21 +6,23 @@ class Video:
 
     def __init__(self, video_id: str):
         self.video_id = video_id
-        self.video_title = self.get_video_info()['items'][0]['snippet']['title']
+        self.items = Video.get_video_info(self.video_id)['items'][0]
+        self.video_title = self.items['snippet']['title']
         self.video_url = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.view_count = self.get_video_info()['items'][0]['statistics']['viewCount']
-        self.like_count = self.get_video_info()['items'][0]['statistics']['likeCount']
+        self.view_count = self.items['statistics']['viewCount']
+        self.like_count = self.items['statistics']['likeCount']
 
     def __str__(self):
         return f'{self.video_title}'
 
-    def get_video_info(self) -> dict:
+    @classmethod
+    def get_video_info(cls, vid_id) -> dict:
         """
         Получает расширенную информацию о видео через API,
         обращаясь к Youtube Data API.
         :return:  Возвращает словарь с указанными данными о видео
         """
-        request = self.service.videos().list(part='snippet, statistics', id=self.video_id)
+        request = cls.service.videos().list(part='snippet, contentDetails, statistics', id=vid_id)
         response = request.execute()
 
         return response
